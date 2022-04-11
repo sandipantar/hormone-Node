39da434c51a73fc292fb15f82cbc86ef66ec44f6
sandipantar <sandipantar@gmail.com>
2022-04-11 22:09:45 +0530

    Delete src directory

--- src/auth/auth.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Admin } from './auth.model';
-
-@Injectable()
-export class AuthService {
-}
