3774dbb6f5b066e2955b23bb6849ad6222bdf20d
sayantan <flaaman@gmail.com>
2022-04-13 09:57:59 +0530

    dfd

--- src/enrolledcourse/enrolledcourse.module.ts
+++ src/enrolledcourse/enrolledcourse.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EnrolledcourseController } from './enrolledcourse.controller';
 import { EnrolledcourseService } from './enrolledcourse.service';
+import { EnrolledcourseSchema } from './enrolledcourse.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{
+      name:'Enrolledcourse', 
+      schema:EnrolledcourseSchema,
+      collection :'enrolledcourse'
+    }]), 
+  ],
   controllers: [EnrolledcourseController],
   providers: [EnrolledcourseService]
 })
